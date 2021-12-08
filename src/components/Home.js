805e6383fc9d3b432c6edf59ab19a58d2889c8ff
RajuRk <45646335+RajuRk@users.noreply.github.com>
2021-12-08 22:28:48 +0530

    code updation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, {Fragment, useContext} from "react";
+import { FoodContext } from "../App";
+import {Link} from 'react-router-dom';
 
 function Home() {
-  return <div>Home page</div>;
+  let context = useContext(FoodContext);
+  return(
+    <div className="home-wrapper">
+      {
+        context.data.map((e,i) => {
+          return <Fragment key={i}>
+            <Link to={`/`+e.name.toLowerCase()} className="item-link">
+              <div className="item-wrapper">
+                <img src={e.image} className="item-image"/>
+                <div className="item-name">{e.name}</div>
+              </div>  
+            </Link> 
+          </Fragment>
+        })
+      }
+    </div>
+  );
 }
 
 export default Home;
