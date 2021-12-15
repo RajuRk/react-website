a10fcfd4cbbc0c8a90f67bd5d99510d5e4d5745c
RajuRk <45646335+RajuRk@users.noreply.github.com>
2021-12-15 16:19:14 +0530

    Updated

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
+import { FoodContext } from "../App";
+import {useNavigate} from 'react-router-dom';
 
 function Burger() {
-  return <div>Burger Works</div>;
+  let context = useContext(FoodContext);
+  console.log(context);
+  let[proucts, setProducts] = useState([]);
+  let navigate = useNavigate();
+  let getData = () => {
+    if(context.data.length > 0)
+      setProducts(context.data[1].subItemsData.subItems)
+    else
+      navigate('/')
+  }
+  useEffect(() => {
+    getData()
+  }, []);
+  
+  return (
+    <div className="product-wrapper">
+      <h2>Crispy Burger</h2>
+      {
+        proucts.map((e,i) => {
+          return <div className="product-item-warpper" key={i}>
+            <div className="product-details">
+              <h4>{e.name}</h4>
+              <div className="product-price">&#x20B9; {e.price}</div>
+              <div className="product-description">{e.description}</div>
+              <button className="product-btn" onClick={()=> {
+                context.cart.push(e);
+                context.setcartValue(context.cart.length)
+              }}>Order Now</button>
+            </div>
+            <div class="product-image">
+              <img src={e.image} alt={e.name}/>
+            </div>
+          </div>
+        })
+      }
+    </div>
+  );
 }
 
 export default Burger;
